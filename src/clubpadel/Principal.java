3a703bbfbe988de24816f2b24aa8d747266cff2b
Susana Rosa <susana.rosa@murciaeduca.es>
2020-12-29 10:46:58 +0100

    primer commit

---
+++ src/clubpadel/Principal.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clubpadel;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Soro
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce el código de socio: ");
+        int codigoSocio = sc.nextInt();
+        sc.nextLine();
+        
+        System.out.print("Introduce nombre y apellidos: ");
+        String nomApeSocio = sc.nextLine();
+        
+        System.out.print("Introduce la edad: ");
+        byte edadSocio = sc.nextByte();
+        sc.nextLine();
+        
+        System.out.print("Introduce un teléfono: ");
+        String telSocio = sc.nextLine();
+        
+        System.out.print("Introduce un correo-e: ");
+        String correoESocio = sc.nextLine();
+        
+        System.out.print("¿Los padres son socios? (S/N)");
+        char respuesta = sc.next().charAt(0);
+        sc.nextLine();
+        
+        boolean padresSocios;
+        if (respuesta == 's' || respuesta == 'S') 
+            padresSocios = true;
+        else
+            padresSocios = false;
+        
+        SocioClubPadel socio1 = new SocioClubPadel(codigoSocio,nomApeSocio,edadSocio, telSocio, correoESocio, padresSocios);
+        
+        SocioClubPadel socio2 = new SocioClubPadel(123, "Perico", (byte)45, "87854565", "pepe@slkdfjs", true);
+        
+        System.out.println(socio1.toString());
+        System.out.println(socio2.toString());
+    }
+    
+}
---
+++ src/clubpadel/SocioClubPadel.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clubpadel;
+
+/**
+ *
+ * @author Soro
+ */
+public class SocioClubPadel {
+    private int codigo;
+    private String nombreApellidos;
+    private byte edad;
+    private String telefono;
+    private String correoE;
+    private boolean padresSonSocios;
+
+    public SocioClubPadel(int codigo, String nombreApellidos, byte edad, String telefono, String correoE, boolean padresSonSocios) {
+        this.codigo = codigo;
+        this.nombreApellidos = nombreApellidos;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.correoE = correoE;
+        this.padresSonSocios = padresSonSocios;
+    }
+
+    public SocioClubPadel() {
+    }
+
+    @Override
+    public String toString() {
+        String salida;
+        
+        salida = "Datos del socio\n";
+        salida += "----------------------------\n";
+        salida += "Código: " + this.codigo + "\n";
+        salida += "Nombre: " + this.nombreApellidos + "\n";
+        salida += "Edad: " + this.edad + "\n";
+        salida += "Telefono: " + this.telefono + "\n";
+        salida += "Correo-e: " + this.correoE + "\n";
+        
+        return salida;
+    }
+    
+    
+    
+    
+    // Getters y Setters
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombreApellidos() {
+        return nombreApellidos;
+    }
+
+    public void setNombreApellidos(String nombreApellidos) {
+        this.nombreApellidos = nombreApellidos;
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoE() {
+        return correoE;
+    }
+
+    public void setCorreoE(String correoE) {
+        this.correoE = correoE;
+    }
+
+    public boolean isPadresSonSocios() {
+        return padresSonSocios;
+    }
+
+    public void setPadresSonSocios(boolean padresSonSocios) {
+        this.padresSonSocios = padresSonSocios;
+    }
+    
+    
+    
+}
