e05bda98b84e8da642d3741a2c2a7fabefa26607
susanarosa <76688891+susanarosa@users.noreply.github.com>
2020-12-30 10:09:09 +0100

    Update Principal.java

--- src/clubpadel/Principal.java
+++ src/clubpadel/Principal.java
@@ -19,7 +19,7 @@ public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Prueba de Fetch");
-        System.out.println("Cambios de Susana ");
+        System.out.println("Cambios de Susana ************ soy el original");
         System.out.print("Introduce el código de socio: ");
         int codigoSocio = sc.nextInt();
         sc.nextLine();
