98c3aa933c4620e5a244117877c8607a6fd9704e
irene-daw <85477425+irene-daw@users.noreply.github.com>
2021-06-15 09:38:25 +0200

    He añadido un atributo surname, un contructor y el método toString() en la Clase Persona

--- src/clubpadel/Persona.java
+++ src/clubpadel/Persona.java
@@ -10,7 +10,21 @@ package clubpadel;
  * @author SUSANA
  */
 public class Persona {
+    
     String name;
+    String surname;
     String dni;
-int edad;
+    int edad;
+
+    public Persona(String name, String surname, String dni, int edad) {
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + name + "\nApellidos: " + surname + "\nDNI: " + dni + "\nEdad: " + edad;
+    }
 }
